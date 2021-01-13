9157bbcdf220a74ec467d0ce88ebca7c1e5980c1
twotwo2 <1372531933@qq.com>
2021-01-13 14:01:49 +0800

    Update __init__.py

--- toolkit/datasets/__init__.py
+++ toolkit/datasets/__init__.py
@@ -1,9 +1,8 @@
-from .vot import VOTDataset, VOTLTDataset
 from .otb import OTBDataset
 from .uav import UAVDataset
 from .lasot import LaSOTDataset
-from .trackingnet import TrackingNetDataset
 from .got10k import GOT10kDataset
+
 
 class DatasetFactory(object):
     @staticmethod
@@ -25,12 +24,6 @@ class DatasetFactory(object):
             dataset = LaSOTDataset(**kwargs)
         elif 'UAV' in name:
             dataset = UAVDataset(**kwargs)
-        elif 'VOT2018' == name or 'VOT2016' == name or 'VOT2019' == name:
-            dataset = VOTDataset(**kwargs)
-        elif 'VOT2018-LT' == name:
-            dataset = VOTLTDataset(**kwargs)
-        elif 'TrackingNet' == name:
-            dataset = TrackingNetDataset(**kwargs)
         elif 'GOT-10k' == name:
             dataset = GOT10kDataset(**kwargs)
         else:
